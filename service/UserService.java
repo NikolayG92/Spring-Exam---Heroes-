6510cb44ef0eab4d3c7b2d3ea92816c7008cb313
nikolayg92 <apokalipsys@mail.bg>
2020-06-24 14:45:35 +0300

    Upload Src Folder

--- service/UserService.java
+++
@@ -1,9 +0,0 @@
-package heroesproject.demo.service;
-
-import heroesproject.demo.models.service.UserServiceModel;
-
-public interface UserService {
-    void register(UserServiceModel map);
-
-    UserServiceModel findByUsername(String username);
-}
